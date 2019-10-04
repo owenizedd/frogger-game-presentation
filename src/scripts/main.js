01b08b635a33d010ea9ae99dbbe1db289fcf04ba
Owen <owenizedd@gmail.com>
2019-10-04 09:24:36 +0700

    Initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke');
+var classes = require('bespoke-classes');
+var nav = require('bespoke-nav');
+var scale = require('bespoke-scale');
+var bullets = require('bespoke-bullets');
+var hash = require('bespoke-hash');
+var prism = require('bespoke-prism');
+var extern = require('bespoke-extern');
+
+// Bespoke.js
+bespoke.from({ parent: 'article.deck', slides: 'section' }, [
+  classes(),
+  nav(),
+  scale('transform'),
+  bullets('.build, .build-items > *'),
+  hash(),
+  prism(),
+  extern(bespoke)
+]);
